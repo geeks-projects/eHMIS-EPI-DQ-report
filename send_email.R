7f2dfddefccd7b5fb4494a05f9cc204cd8080ba4
Amanyiraho <amanyiraho@gmail.com>
2024-02-02 10:02:32 +0300

    send email via GHA

---
+++ send_email.R
@@ -0,0 +1,24 @@
+library(blastula)
+
+my_email_object <- compose_email(
+  body = 
+    "Hello,
+  
+  Please receive the Q4 2023 EPI eHMIS report
+  
+  Cheers,
+  
+  GEEKS" )
+
+my_email_object |> 
+  #add_attachment(file = "eHMIS-EPI-DQ-report.pdf") |> 
+  smtp_send(
+    from = "ugandarug@gmail.com",
+    to = "amanyiraho@gmail.com",
+    subject = "Q4 2023 EPI eHMIS report",
+    credentials = creds_envvar( user = "ugandarug@gmail.com",
+                                pass_envvar = Sys.getenv("SMTP_PASSWORD"),
+                                provider = "gmail"
+                              )
+    
+  ) 
