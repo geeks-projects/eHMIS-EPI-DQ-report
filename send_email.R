c2de89b2fe12413a12277adcd6783eb2afbfe21c
Amanyiraho <amanyiraho@gmail.com>
2024-02-02 10:42:14 +0300

    hide everything

--- send_email.R
+++ send_email.R
@@ -1,5 +1,22 @@
 library(blastula)
 
+
+# read environment variables ----------------------------------------------
+EMAIL_SENDER <- Sys.getenv("EMAIL_SENDER")
+SMTP_PASSWORD <- Sys.getenv("SMTP_PASSWORD")
+EMAIL_RECIPIENT <- Sys.getenv("EMAIL_RECIPIENT")
+
+# set gmail credentials ---------------------------------------------------
+credentials <- creds_envvar(
+  user = EMAIL_SENDER,
+  pass_envvar = "SMTP_PASSWORD",
+  provider = "gmail",
+  host = NULL,
+  port = NULL,
+  use_ssl = TRUE
+)
+
+
 my_email_object <- compose_email(
   body = 
     "Hello,
@@ -10,15 +27,17 @@ my_email_object <- compose_email(
   
   GEEKS" )
 
+
+
 my_email_object |> 
   #add_attachment(file = "eHMIS-EPI-DQ-report.pdf") |> 
   smtp_send(
-    from = "ugandarug@gmail.com",
-    to = "amanyiraho@gmail.com",
+    from = EMAIL_SENDER,
+    to = EMAIL_RECIPIENT,
+   # from = "ugandarug@gmail.com", # i did user quotes
+   # to = "amanyiraho@gmail.com",
     subject = "Q4 2023 EPI eHMIS report",
-    credentials = creds_envvar( user = "ugandarug@gmail.com",
-                                pass_envvar = "SMTP_PASSWORD",
-                                provider = "gmail"
-                              )
+    credentials = credentials, 
+    verbose = TRUE
     
   ) 
