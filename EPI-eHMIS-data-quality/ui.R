b7c73eea700afb20d722cef085bbb9ebe7a5cfec
Amanyiraho <amanyiraho@gmail.com>
2024-03-01 06:29:36 +0300

    :

---
+++ EPI-eHMIS-data-quality/ui.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(dplyr)
+library(scales)
+library(sf)
+library(ggspatial)
+library(shiny)
+library(reactable)
+library(vroom)
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+navbarPage(
+  "Page title",   
+  tabPanel("National", "one"),
+  tabPanel("District", "two")
+  )
+)
